72ded3f1ee6ef4cf520bea6f44ee2bb4d32a57c3
Yash Jain <yaashjainn@gmail.com>
2022-09-22 09:05:51 +0530

    initial commit - project setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Search Pannel for movies
+
+# Run The project
+
+# git clone https://github.com/yash560/search-backend.git
+
+# Inside the project dir
+
+# run npm install or npm install --force
+
+# then npm start (The server will start at default localhost 2000)
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+
+const path = require("path");
+var cors = require("cors");
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(cors());
+
+app.use(express.json()); // to accept json data
+
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+
+// --------------------------deployment------------------------------
+
+// Error Handling and auth middlewares
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
